6f3458c5376e240c05f014ddcc3d09294085f25d
Klizlo <klizlo0111@gmail.com>
2023-12-12 16:31:32 +0100

    Initial Commit

---
+++ Connections/ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsApp.swift
+//  Connections
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
